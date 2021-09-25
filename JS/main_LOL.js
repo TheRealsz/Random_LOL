16ea978f62666ee1ce4a9464790ca9d84cca9813
unknown <Robindiegoa@gmail.com>
2021-09-25 15:46:35 -0300

    ajustes nos scripts e nos links

--- JS/main_LOL.js
+++ JS/main_LOL.js
@@ -1,7 +1,4 @@
-// import express from 'express';
-// var app = express()
 
-// app.listen(process.env.PORT || 8080)
 
 // let url = `http://ddragon.leagueoflegends.com/cdn/11.18.1/data/pt_BR/champion/${championName}.json`;
 let urlListChamp;
